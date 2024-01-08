9ee977780880ee3a0445f040e2b43f93f46b1f0a
ChangHoon Choi <69202388+hoon25@users.noreply.github.com>
2024-01-08 21:14:44 +0900

    TAS-11-스프링기본 설정 (#1)
    * feat(spring): init
    
    * feat(gradle): lombok 추가
    
    * feat(gradle): jpa setting 추가

---
+++ thanksbucket/src/main/java/com/thanksbucket/ThanksbucketApplication.java
@@ -0,0 +1,13 @@
+package com.thanksbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanksbucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanksbucketApplication.class, args);
+	}
+
+}
---
+++ thanksbucket/src/test/java/com/thanksbucket/ThanksbucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanksbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanksbucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
