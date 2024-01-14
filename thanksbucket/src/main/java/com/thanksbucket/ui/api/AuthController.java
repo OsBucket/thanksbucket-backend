5712ca8e745827d8835211e1126b22a6e4137baa
hoon25 <cchoon95@hanmail.net>
2024-01-14 21:44:45 +0900

    feat(member): logout, current 기능 추가

--- thanksbucket/src/main/java/com/thanksbucket/ui/api/AuthController.java
+++ thanksbucket/src/main/java/com/thanksbucket/ui/api/AuthController.java
@@ -2,9 +2,13 @@ package com.thanksbucket.ui.api;
 
 import com.thanksbucket.application.AuthService;
 import com.thanksbucket.ui.dto.SignupRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +24,18 @@ public class AuthController {
     public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequest request) {
         authService.signup(request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(HttpSession session) {
+        if(session != null) {
+            session.invalidate();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<String> current(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(user.getUsername());
     }
 }
