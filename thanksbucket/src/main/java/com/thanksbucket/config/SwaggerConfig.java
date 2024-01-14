ad3b5de9fe025e0eff0b779a0e785bcfff8878df
hoon25 <cchoon95@hanmail.net>
2024-01-14 22:21:44 +0900

    feat(swagger): swagger JSESSIONID추가

--- thanksbucket/src/main/java/com/thanksbucket/config/SwaggerConfig.java
+++ thanksbucket/src/main/java/com/thanksbucket/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.thanksbucket.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,10 +14,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("ThanksBucket API")
+                .description("ThanksBucket API")
+                .version("v1");
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.COOKIE)
+                .name("JSESSIONID");
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("JSESSIONID");
+
         return new OpenAPI()
-                .info(new Info().title("ThanksBucket API")
-                        .description("ThanksBucket API")
-                        .version("v1")
-                );
+                .components(new Components().addSecuritySchemes("JSESSIONID", securityScheme))
+                .addSecurityItem(securityRequirement)
+                .info(info);
     }
 }
