cde828fb53c5ce839ee64523fd1b15fdb237ee07
hoon25 <cchoon95@hanmail.net>
2024-01-16 01:37:32 +0900

    feat(Bucket): Bucket Entity 생성

---
+++ thanksbucket/src/main/java/com/thanksbucket/domain/bucket/Bucket.java
@@ -0,0 +1,30 @@
+package com.thanksbucket.domain.bucket;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "buckets")
+public class Bucket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private LocalDate startDate;
+}
