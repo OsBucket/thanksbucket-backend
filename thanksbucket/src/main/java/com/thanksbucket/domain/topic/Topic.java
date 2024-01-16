78891dd1787b3ecbec7abdde63af6e3dc00c20fd
hoon25 <cchoon95@hanmail.net>
2024-01-16 21:40:30 +0900

    feat(topics): entity 추가

---
+++ thanksbucket/src/main/java/com/thanksbucket/domain/topic/Topic.java
@@ -0,0 +1,29 @@
+package com.thanksbucket.domain.topic;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@Table(name = "topics")
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false, unique = true)
+    private String content;
+}
