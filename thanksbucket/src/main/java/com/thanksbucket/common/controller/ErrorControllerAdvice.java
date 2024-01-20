704f92154faeade0eeef262a7c75284940c444f2
hoon25 <cchoon95@hanmail.net>
2024-01-21 03:11:13 +0900

    feat(ControllerAdvice): validation 응답 추가

--- thanksbucket/src/main/java/com/thanksbucket/common/controller/ErrorControllerAdvice.java
+++ thanksbucket/src/main/java/com/thanksbucket/common/controller/ErrorControllerAdvice.java
@@ -2,6 +2,8 @@ package com.thanksbucket.common.controller;
 
 import com.thanksbucket.common.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,18 @@ public class ErrorControllerAdvice {
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.builder()
                         .message(e.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> invalidRequestHandler(MethodArgumentNotValidException e) {
+        StringBuilder sb = new StringBuilder();
+        for (FieldError fieldError: e.getFieldErrors()){
+            sb.append(String.format("%s : %s \n", fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.builder()
+                        .message(sb.toString())
                         .build());
     }
 }
