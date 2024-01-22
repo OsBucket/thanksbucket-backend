91e77091bb5f2241f37962ac47f3a5b8c3a493cd
hoon25 <cchoon95@hanmail.net>
2024-01-23 01:01:53 +0900

    feat(log): error 시 로그 추가

--- thanksbucket/src/main/java/com/thanksbucket/common/controller/ErrorControllerAdvice.java
+++ thanksbucket/src/main/java/com/thanksbucket/common/controller/ErrorControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.thanksbucket.common.controller;
 
 import com.thanksbucket.common.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.thanksbucket")
+@Slf4j
 public class ErrorControllerAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.builder()
                         .message(e.getMessage())
