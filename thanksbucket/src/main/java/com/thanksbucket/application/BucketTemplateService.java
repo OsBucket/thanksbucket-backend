6c6ac22ca7babc7b9cefdf31ec62007f0c52eb68
hoon25 <cchoon95@hanmail.net>
2024-03-04 01:24:13 +0900

    feat(BucketTemplate): 이름검색기능 추가

---
+++ thanksbucket/src/main/java/com/thanksbucket/application/BucketTemplateService.java
@@ -0,0 +1,20 @@
+package com.thanksbucket.application;
+
+import com.thanksbucket.domain.buckettemplate.BucketTemplate;
+import com.thanksbucket.domain.buckettemplate.BucketTemplateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BucketTemplateService {
+    private final BucketTemplateRepository bucketTemplateRepository;
+
+    public List<BucketTemplate> findByBucketName(String bucketName) {
+        return bucketTemplateRepository.findBucketTemplateByBucketNameContaining(bucketName);
+    }
+}
