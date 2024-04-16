63d882c564e148b495053c814aebdc1d05d719f5
hoon25 <cchoon95@hanmail.net>
2024-04-16 21:34:03 +0900

    feat: add BaseTimeEntity

---
+++ thanksbucket/src/main/java/com/thanksbucket/domain/common/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.thanksbucket.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime lastModifiedDate;
+}
