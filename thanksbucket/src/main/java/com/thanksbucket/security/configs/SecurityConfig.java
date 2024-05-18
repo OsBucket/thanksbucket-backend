8e340ece35771662e9f95ac5badb8c1452ef2beb
hoon25 <cchoon95@hanmail.net>
2024-05-19 01:29:20 +0900

    feat(swagger): path 변경 및 redirect 설정

--- thanksbucket/src/main/java/com/thanksbucket/security/configs/SecurityConfig.java
+++ thanksbucket/src/main/java/com/thanksbucket/security/configs/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/api/health").permitAll()
+                        .requestMatchers("/", "/api/health", "/api").permitAll()
                         .requestMatchers("/api/auth/login", "/api/auth/signup", "/api/occupations", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/**").hasRole("USER")
                         .anyRequest()
--- thanksbucket/src/main/java/com/thanksbucket/ui/api/HomeController.java
+++ thanksbucket/src/main/java/com/thanksbucket/ui/api/HomeController.java
@@ -2,14 +2,33 @@ package com.thanksbucket.ui.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class HomeController {
+
+    @Value("${springdoc.swagger-ui.path}")
+    private String swaggerPath;
+
+
+    @GetMapping("/")
+    public ResponseEntity<Object> home() {
+        log.debug("redirect to swagger");
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create(swaggerPath));
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .headers(httpHeaders)
+                .build();
+    }
 
     @GetMapping("/api/health")
     public ResponseEntity<String> health() {
