d49dd171267754096c1b07cac1475ed4e8aa163c
hoon25 <cchoon95@hanmail.net>
2024-05-19 22:33:08 +0900

    feat(Member): OAuth2.0관련 컬럼 추가

--- thanksbucket/src/main/java/com/thanksbucket/domain/member/Member.java
+++ thanksbucket/src/main/java/com/thanksbucket/domain/member/Member.java
@@ -6,6 +6,8 @@ import com.thanksbucket.domain.occupation.Occupation;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -43,7 +45,16 @@ public class Member extends BaseTimeEntity {
     private String nickname;
 
     @Column
+    private String imageUrl;
+
+    @Column
     private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Bucket> buckets = new ArrayList<>();
