d75f2e2a9dcb75095f73ff656be64c8de91a405b
hoon25 <cchoon95@hanmail.net>
2024-05-21 21:18:07 +0900

    feat: 소셜 가입 후 추가정보 생성 구현

--- thanksbucket/src/main/java/com/thanksbucket/application/AuthService.java
+++ thanksbucket/src/main/java/com/thanksbucket/application/AuthService.java
@@ -8,7 +8,6 @@ import com.thanksbucket.security.oauth2.userinfo.OAuth2UserInfo;
 import com.thanksbucket.slack.SlackService;
 import com.thanksbucket.ui.dto.SignupRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,26 +15,18 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final MemberRepository memberRepository;
     private final OccupationRepository occupationRepository;
-    private final PasswordEncoder passwordEncoder;
     private final SlackService slackService;
 
-    public Long signup(SignupRequest request) {
-        memberRepository.findByMemberId(request.getMemberId())
-                .ifPresent(member -> {
-                    throw new IllegalArgumentException("이미 존재하는 회원입니다.");
-                });
+    public Long signup(SignupRequest request, String email) {
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("인증에 성공한 유저가 존재하지 않습니다."));
         if (request.getOccupationId() != null) {
             Occupation occupation = occupationRepository.findById(request.getOccupationId())
                     .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 직업입니다."));
-            Member member = Member.signup(passwordEncoder, request.getMemberId(), request.getPassword(), request.getNickname(), request.getBirthday(), occupation);
-            Member savedMember = memberRepository.save(member);
-            slackService.sendSignupMessage(savedMember.getNickname());
-            return savedMember.getId();
+            member.updateOccupation(occupation);
         }
-        Member member = Member.signup(passwordEncoder, request.getMemberId(), request.getPassword(), request.getNickname(), request.getBirthday(), null);
-        Member savedMember = memberRepository.save(member);
-        slackService.sendSignupMessage(savedMember.getNickname());
-        return savedMember.getId();
+        member.signup(request.getNickname(), request.getBirthday(), request.getDiscoveryPath());
+        slackService.sendSignupMessage(member.getEmail(), member.getNickname());
+        return member.getId();
     }
 
     public Member findIfNotExistCreateMember(OAuth2UserInfo oAuth2UserInfo) {
