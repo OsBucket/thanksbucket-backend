15dac8c8b4ea4569347a24793b50c0a4f3409c66
hoon25 <cchoon95@hanmail.net>
2024-07-02 00:12:14 +0900

    test: fix auth test

--- thanksbucket/src/main/java/com/thanksbucket/domain/member/Member.java
+++ thanksbucket/src/main/java/com/thanksbucket/domain/member/Member.java
@@ -104,9 +104,10 @@ public class Member extends BaseTimeEntity {
   }
 
   //TODO 삭제 예정
-  public Member(String email, String nickname) {
+  public Member(String email, String nickname, MemberRole memberRole) {
     this.email = email;
     this.nickname = nickname;
+    this.memberRole = memberRole;
   }
 
   public void signup(String nickname, LocalDate birthday, String discoveryPath) {
